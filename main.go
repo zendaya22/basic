1133a6bf85bb4edefd6c946a1a57806ab25d1f28
deemisterious <dimasixc22@gmail.com>
2024-08-06 21:30:40 +0700

    zero values

--- main.go
+++ main.go
@@ -3,31 +3,13 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("hello ")
-
-	const agency string = "fast track"
-	var name string = "dimas"
-
-	// another variabel
-
-	user_name := "dimas"
-
-	// integer
-
-	var total_cars int = 50
-
-	// float aouto
-
-	startingPrice := 29.99
-
-	fmt.Println("hello", name)
-	fmt.Println("welcome to",agency)
-	fmt.Println("user name", user_name)
-	fmt.Println("Out fleet consists of", total_cars, "cars")
-	fmt.Printf("Our prices start at %v\n", startingPrice)
-	fmt.Println("take your pick")
-
-	insuranceIncluded := false
-	fmt.Println(insuranceIncluded)
 	
+	var name string
+	fmt.Println("name", name)
+	var count int
+	fmt.Println("count", count)
+	var price float64
+	fmt.Printf("price %.1f\n", price)
+	var insured bool
+	fmt.Println("insured", insured)	
 }
