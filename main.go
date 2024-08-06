01932ae5b55f5163a038faabd35abff74b430605
deemisterious <dimasixc22@gmail.com>
2024-08-06 22:09:17 +0700

    42 : 47

--- main.go
+++ main.go
@@ -12,5 +12,9 @@ func main() {
 	// cast data type 
 	k = float64(c) + 273.15
 	fmt.Println(k)
+	var tempInt int = 10
+	var tempFloat float64 = float64(tempInt)
+	fmt.Println(tempFloat)
+	fmt.Printf("integer to float %.1f\n", 10.0)
 
 }
