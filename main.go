5c280f8ccb4936f50fc128b60e873eea2add1fa0
deemisterious <dimasixc22@gmail.com>
2024-08-06 22:01:02 +0700

    math and cast data type

--- main.go
+++ main.go
@@ -2,31 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func main(){
-	
-	str := "Hello, World!"
-	length := len(str)
-	fmt.Println(length)
+func main() {
 
-	str1 := "dimas"
-	str2 := "DIMAs"
-	// compare
-	fmt.Println(strings.EqualFold(str1, str2))
-	// index in string
-	fmt.Println(strings.Index(str, "H"))
-	// substring
-	subs := str[strings.Index(str, "H"): 5]
-	fmt.Println(subs)
-	a := str[:13]
-	b := str[:]
-	fmt.Println(a)
-	fmt.Println(b)
-	// upper and lowwer
-	str6 := "Go for web development"
-	fmt.Println(strings.ToLower(str6))
-	fmt.Println(strings.ToUpper(str6))
-	// contain to check 
+	// math
+	c := 20
+	k := 0.0
+	// cast data type 
+	k = float64(c) + 273.15
+	fmt.Println(k)
+
 }
