68810af5234747b0f41e0195a94e1286c4b799ed
deemisterious <dimasixc22@gmail.com>
2024-08-06 21:23:01 +0700

    variabel str and int

--- main.go
+++ main.go
@@ -4,4 +4,26 @@ import "fmt"
 
 func main(){
 	fmt.Println("hello ")
+
+	const agency string = "fast track"
+	var name string = "dimas"
+
+	// another variabel
+
+	user_name := "dimas"
+
+	// integer
+
+	var total_cars int = 50
+
+	// float aouto
+
+	startingPrice := 29.99
+
+	fmt.Println("hello", name)
+	fmt.Println("welcome to",agency)
+	fmt.Println("user name", user_name)
+	fmt.Println("Out fleet consists of", total_cars, "cars")
+	fmt.Printf("Our prices start at %v\n", startingPrice)
+	fmt.Println("take your pick")
 }
