15dde6ef8534b2565e60c3cda285b4e5e77f16e8
dimas <dimasixc22@gmail.com>
2024-08-19 21:09:30 +0700

    waitgrups

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"time"
+	"sync"
 )
 
 
-func speak(total int, message string){
+func speak(total int, message string, wg *sync.WaitGroup){
+
+	if wg != nil {
+		defer wg.Done()
+	}
+
 	for i := 0; i < total; i++ {
 		fmt.Println("Speak", message, "- (", i+1, ")")
 	}
@@ -15,12 +20,19 @@ func speak(total int, message string){
 
 func main() {
 
+	wg := sync.WaitGroup{}
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	fmt.Println("running goroutine", runtime.NumCPU(), "cpu")
+	wg.Add(3)
+	go speak(2, "goroutine 1", &wg)
+	go speak(2, "goroutine 2", &wg)
+	go speak(2, "goroutine 3", &wg)
 	
-	go speak(2, "goroutine 1")
-	go speak(2, "goroutine 2")
-	go speak(2, "goroutine 3")
-	time.Sleep(1 * time.Second)
+
+	wg.Wait()
+
+	fmt.Println("done")
+
 
 }
